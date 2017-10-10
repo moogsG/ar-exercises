c4f9a431c444aa04b755e01e6e2fd3ffd8fd271a
Morgan Greff <morgan@greffs.com>
2017-10-10 20:06:41 +0000

    Finished Question seven

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,8 @@
 class Employee < ActiveRecord::Base
+  belongs_to :store
 
+  validates :first_name, :last_name, presence: true
+  validates :hourly_rate, numericality: { less_than_or_equal_to: 200 }
+  validates :hourly_rate, numericality: { greater_than: 39 }
+  validates :store, presence: true
 end
